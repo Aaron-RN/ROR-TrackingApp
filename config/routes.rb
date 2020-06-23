c7ad212b8cf1015d0429a30ee86996c704da69b9
Aaron-RN <aaron.rory.newbold@gmail.com>
2020-06-23 12:35:23 -0400

    added sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'sessions/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :sessions, only: [:create]
   namespace 'api' do
     namespace 'v1' do
     end
