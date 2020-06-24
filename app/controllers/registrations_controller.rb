143d81f07f4c1445a338dfcd0b245f461c1a45b2
Aaron Rory Newbold <aaron.rory.newbold@gmail.com>
2020-06-24 19:20:51 -0400

    Changed User.create!() method to User.new() method

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,13 +1,13 @@
 class RegistrationsController < ApplicationController
   def create
-    user = User.create!(
+    user = User.new(
       username: params['user']['username'],
       email: params['user']['email'],
       password: params['user']['password'],
       password_confirmation: params['user']['password_confirmation']
     )
 
-    if user
+    if user.save
       session[:user_id] = user.id
       render json: {
         status: :created,
