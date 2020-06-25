fb89c0d95a3986a6cd084a1cdab0e16cf77ec254
Aaron Rory Newbold <aaron.rory.newbold@gmail.com>
2020-06-25 18:41:17 -0400

    Added before_destroy action
    Before_destroy action erases all related notes before destroying the food item

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,4 +2,11 @@ class Food < ApplicationRecord
   belongs_to :user
   has_many :notes, dependent: :destroy
   validates :name, length: { in: 2..60 }, presence: true
+  before_destroy :destroy_notes
+  
+  private
+  
+  def destroy_notes
+    notes.destroy_all
+  end
 end
