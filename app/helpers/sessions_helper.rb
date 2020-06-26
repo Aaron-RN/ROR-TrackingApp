1014f766dd575f05785659022f8bb9b31b09790a
Aaron-RN <aaron.rory.newbold@gmail.com>
2020-06-26 12:51:46 -0400

    debugging session helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,9 @@
 module SessionsHelper
   def set_current_user
+    puts "Session ID: #{session[:user_id]}"
     if session[:user_id]
       @current_user = User.find(session[:user_id])
+      puts "@Current User = #{@current_user}"
     else
       render json: {
         status: 401,
