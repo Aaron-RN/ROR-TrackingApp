2c39dd63656d1cead0af7114962dce8a77de9ee8
Aaron-RN <aaron.rory.newbold@gmail.com>
2020-06-26 13:45:21 -0400

    debugging session helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,15 +1,17 @@
 module SessionsHelper
+  def clean_session
+    reset_session
+    render json: {
+      status: 200,
+      logged_out: true
+    }
+  end
+
   def set_current_user
     if session[:user_id]
       @current_user = User.find(session[:user_id])
 
-      unless @current_user
-        reset_session
-        render json: {
-          status: 200,
-          logged_out: true
-        }
-      end
+      rescue_from ActiveRecord::RecordNotFound, with: :clean_session
     else
       render json: {
         status: 401,
