c42f3a0c0bdc3befc7c5dff468b914bb367c184a
Aaron-RN <aaron.rory.newbold@gmail.com>
2020-06-26 13:08:43 -0400

    debugging session helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,6 @@
 module SessionsHelper
   def set_current_user
+    reset_session
     puts "Session ID: #{session[:user_id]}"
     if session[:user_id]
       @current_user = User.find(session[:user_id])
