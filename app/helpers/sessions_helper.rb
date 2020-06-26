87f5a731201185bb4c656bc4f66d7ffa353fe55a
Aaron Rory Newbold <aaron.rory.newbold@gmail.com>
2020-06-26 18:19:46 -0400

    switched to return unless statement

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,20 +1,21 @@
 module SessionsHelper
   def set_current_user
-    if session[:user_id]
-      @current_user = User.find_by(id: session[:user_id])
-      if @current_user.nil?
-        reset_session
-        render json: {
-          status: 401,
-          error: ['Not logged in!'],
-          logged_out: true
-        }
-      end
-    else
+    return unless session[:user_id]
+    
+    @current_user = User.find_by(id: session[:user_id])
+    if @current_user.nil?
+      reset_session
       render json: {
         status: 401,
-        error: ['Not logged in']
+        error: ['Not logged in!'],
+        logged_out: true
       }
     end
+#     else
+#       render json: {
+#         status: 401,
+#         error: ['Not logged in']
+#       }
+#     end
   end
 end
