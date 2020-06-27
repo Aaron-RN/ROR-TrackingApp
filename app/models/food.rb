a4482b0d8a3fb1dbb6642f7134bc69300a983e6c
Aaron Rory Newbold <aaron.rory.newbold@gmail.com>
2020-06-27 09:57:59 -0400

    Added validations for all fields
    except date_consumed

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,10 +2,14 @@ class Food < ApplicationRecord
   belongs_to :user
   has_many :notes, dependent: :destroy
   validates :name, length: { in: 2..60 }, presence: true
+  validates :servings_consumed, numericality: { greater_than_or_equal_to: 1 }
+  validates :carbs, numericality: { greater_than_or_equal_to: 0 }
+  validates :fats, numericality: { greater_than_or_equal_to: 0 }
+  validates :proteins, numericality: { greater_than_or_equal_to: 0 }
   before_destroy :destroy_notes
 
   private
-
+  
   def destroy_notes
     notes.destroy_all
   end
