8220b10d05513d69b8296dc2edd586045dad8639
Aaron Rory Newbold <aaron.rory.newbold@gmail.com>
2020-07-06 16:38:24 -0400

    debug

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -62,7 +62,9 @@ class FoodsController < ApplicationController
 
   # Use callbacks to share common setup or constraints between actions.
   def set_food
-    @food = Food.with_notes.select{ |elem|  elem.id == params[:id] }
+    puts Food.with_notes
+    puts ''
+    @food = Food.with_notes.find_by(id: params[:id]).select{ |elem|  elem.id == params[:id] }
   end
 
   def food_params
