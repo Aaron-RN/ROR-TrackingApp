d553df8a52f485fa44e6df89dfb1ff8f06a2e72c
Aaron Rory Newbold <aaron.rory.newbold@gmail.com>
2020-07-08 19:52:57 -0400

    Removed duplicate methods code

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,14 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allowed_methods = i%[get post put patch delete options head]
   allow do
     origins 'http://localhost:3000'
-    resource '*', headers: :any, methods:
-    %i[get post put patch delete options head], credentials: true
+    resource '*', headers: :any, methods: allowed_methods, credentials: true
   end
   allow do
     origins 'https://arn-tracking-app.herokuapp.com'
-    resource '*', headers: :any, methods:
-    %i[get post put patch delete options head], credentials: true
+    resource '*', headers: :any, methods: allowed_methods, credentials: true
   end
 end
