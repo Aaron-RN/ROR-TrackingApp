eea5814d6ba50c50351ec8a7f0d0cd9eec43b38a
Aaron Rory Newbold <aaron.rory.newbold@gmail.com>
2020-07-13 09:27:32 -0400

    minor typo fix
    minor typo for %i

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allowed_methods = i%[get post put patch delete options head]
+  allowed_methods = %i[get post put patch delete options head]
   allow do
     origins 'http://localhost:3000'
     resource '*', headers: :any, methods: allowed_methods, credentials: true
