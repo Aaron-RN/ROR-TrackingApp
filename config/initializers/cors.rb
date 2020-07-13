da380ef264763182ff346e3c38c657663286cdb0
Aaron Rory Newbold <aaron.rory.newbold@gmail.com>
2020-07-13 09:31:41 -0400

    Added condition for rails.env

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,13 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  url = Rails.env == 'production' ? 'https://arn-tracking-app.herokuapp.com' : 'http://localhost:3000'
   allowed_methods = %i[get post put patch delete options head]
   allow do
-    origins 'http://localhost:3000'
-    resource '*', headers: :any, methods: allowed_methods, credentials: true
-  end
-  allow do
-    origins 'https://arn-tracking-app.herokuapp.com'
+    origins url
     resource '*', headers: :any, methods: allowed_methods, credentials: true
   end
 end
